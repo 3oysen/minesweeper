d091710a3d4b28389c10be4257af79f068e0335f
3oysen <103428192+3oysen@users.noreply.github.com>
2023-04-23 18:13:40 +0200

    added difficulty-section, added btn stylesheet

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const menuPlayBtn = document.getElementById("menu-play-btn");
+const difficultySection = document.getElementById("difficulty-section");
 const descriptionBtn = document.getElementById("description-btn");
 const description = document.getElementById("description-section");
 const img = document.getElementById("img");
 
 menuPlayBtn.addEventListener("click", () => {
-	console.log("click");
+	if (difficultySection.style.display === "block") {
+		description.style.display = "none";
+		img.style.display = "none";
+	} else {
+		difficultySection.style.display = "block";
+	}
 });
 
 descriptionBtn.addEventListener("click", () => {
