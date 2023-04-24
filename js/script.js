3b91027a75ea2345d2d8d46f8d8b1176f917b982
3oysen <103428192+3oysen@users.noreply.github.com>
2023-04-24 20:32:49 +0200

    simplified structure of btn and added to new file, fixed menu-play-btn

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
+const menuSection = document.getElementById("menu-section");
 const menuPlayBtn = document.getElementById("menu-play-btn");
-const difficultySection = document.getElementById("difficulty-section");
-const descriptionBtn = document.getElementById("description-btn");
 const description = document.getElementById("description-section");
+const descriptionBtn = document.getElementById("description-btn");
 const img = document.getElementById("img");
+const difficultySection = document.getElementById("difficulty-section");
+const btnEasy = document.getElementById("btn-easy");
+const btnMedium = document.getElementById("btn-medium");
+const btnHard = document.getElementById("btn-hard");
 
 menuPlayBtn.addEventListener("click", () => {
 	if (difficultySection.style.display === "block") {
+		menuPlayBtn.innerHTML = "Play";
 		description.style.display = "none";
-		img.style.display = "none";
+		img.style.display = "block";
+		difficultySection.style.display = "none";
 	} else {
+		menuPlayBtn.innerHTML = "Menu";
+		description.style.display = "none";
+		descriptionBtn.innerHTML = "Description";
+		img.style.display = "none";
 		difficultySection.style.display = "block";
 	}
 });
@@ -19,8 +29,26 @@ descriptionBtn.addEventListener("click", () => {
 		description.style.display = "none";
 		img.style.display = "block";
 	} else {
-		descriptionBtn.innerHTML = "Close description";
+		menuPlayBtn.innerHTML = "Play";
+		descriptionBtn.innerHTML = "Menu";
 		description.style.display = "block";
 		img.style.display = "none";
+		difficultySection.style.display = "none";
 	}
+});
+
+btnEasy.addEventListener("click", () => {
+	menuSection.style.display = "none";
+	description.style.display = "none";
+	difficultySection.style.display = "none";
+	const gameBoard = document.createElement("div");
+	console.log(gameBoard);
+});
+
+btnMedium.addEventListener("click", () => {
+	console.log("medium");
+});
+
+btnHard.addEventListener("click", () => {
+	console.log("hard");
 });
